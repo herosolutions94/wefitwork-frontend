6cc974587cc6cbf2667f7860016b7fd54b27fff8
Naeem <naeem@herosolutions.com.pk>
2023-11-09 17:55:19 +0500

    Cms

--- pages/login.js
+++ pages/login.js
@@ -1,35 +1,61 @@
 import React, { useState} from "react";
 import Link from 'next/link'
-export default function Login() {
+import Text from "../components/text";
+import http from "../helpers/http";
+import MetaGenerator from "../components/meta-generator";
+import Image from "next/image";
+import { cmsFileUrl } from "../helpers/helpers";
+
+export const getServerSideProps = async () => {
+      
+    const result = await http
+      .get("login-page")
+      .then((response) => response.data)
+      .catch((error) => error.response.data.message);
+  
+    return { props: { result } };
+  };
+  
+
+export default function Login({result}) {
+    let { page_title, meta_desc, content, site_settings } = result;
   return (
     <>
+      <MetaGenerator page_title={page_title} meta_desc={meta_desc} />
+
       <main className="logon_main">
         <section className="logon_sec">
             <div className="logon_left">
                 <div className="inner">
-                    <h1>Welcome Back!</h1>
-                    <p>Enjoy the convenience of finding, booking, and communicating with service providers in one place.</p>
+                <h1><Text string={content?.sec1_heading} /></h1>
+                    <Text string={content?.sec1_detail} />
                 </div>
                 <ul>
-                    <li><Link href="/contact">Contact  Us</Link></li>
-                    <li><Link href="/contact">Terms of use</Link></li>
-                    <li><Link href="/contact">Privacy Policy</Link></li>
+                <li><Link href={content?.sec1_button1_link}><Text string={content?.sec1_button1_text} /></Link></li>
+                    <li><Link href={content?.sec1_button2_link}><Text string={content?.sec1_button2_text} /></Link></li>
+                    <li><Link href={content?.sec1_button3_link}><Text string={content?.sec1_button3_text} /></Link></li>
                 </ul>
             </div>
             <div className="logon_right">
                 <div className="login_header">
                     <div className="logon_logo">
                         <Link href="/">
-                            <img src="images/logo_log.svg" alt="" />
+                        <Image 
+                                src={cmsFileUrl(site_settings?.site_logo)}
+                                width={220}
+                                height={40}
+                                alt={site_settings?.site_name}
+                            />
                         </Link>
                     </div>
                     <div className="btn_blk">
-                        <Link href="" className="site_btn">Become a Professional</Link>
+                    <Link href={content?.right_top_button_link} className="site_btn"><Text string={content?.right_top_button_text} /></Link>
+
                     </div>
                 </div>
                 <div className="right_inner">
-                    <h2>Sign in to your account</h2>
-                    <p>Enter your login details below</p>
+                <h2><Text string={content?.right_sec_heading} /></h2>
+                    <p><Text string={content?.right_sec_tagline} /></p>
                     <form>
                         <div className="form_blk">
                             <input
@@ -55,15 +81,15 @@ export default function Login() {
                         <div className="have_check">
                             <div className="lbl_btn">
                                 <input type="checkbox" name="remember" id="remember"/>
-                                <label htmlFor="remember">Remember me</label>
+                                <label htmlFor="remember"><Text string={content?.check_box_text} /></label>
                             </div>
-                            <Link href="/forgot-password">Forgot your password ?</Link>
+                            <Link href="/forgot-password"><Text string={content?.forgot_pswd_text} /></Link>
                         </div>
                         <div className="btn_blk">
-                            <button className="site_btn block">Sign In</button>
+                            <button className="site_btn block"><Text string={content?.button_text} /></button>
                         </div>
                         <div className="question">
-                            <p>Don't have an account? <Link href="/signup">Sign up</Link></p>
+                            <p><Text string={content?.l_link_tagline} /> <Link href={content?.register_link}><Text string={content?.register_link_text} /></Link></p>
                         </div>
                     </form>
                 </div>
--- pages/signup.js
+++ pages/signup.js
@@ -1,35 +1,59 @@
 import React, { useState} from "react";
 import Link from 'next/link'
-export default function Signup() {
+import Text from "../components/text";
+import http from "../helpers/http";
+import MetaGenerator from "../components/meta-generator";
+import Image from "next/image";
+import { cmsFileUrl } from "../helpers/helpers";
+
+export const getServerSideProps = async () => {
+      
+    const result = await http
+      .get("signup-page")
+      .then((response) => response.data)
+      .catch((error) => error.response.data.message);
+  
+    return { props: { result } };
+  };
+
+
+export default function Signup({result}) {
+    let { page_title, meta_desc, content, site_settings } = result;
   return (
     <>
+     <MetaGenerator page_title={page_title} meta_desc={meta_desc} />
       <main className="logon_main">
         <section className="logon_sec">
             <div className="logon_left">
                 <div className="inner">
-                    <h1>Hello Friend</h1>
-                    <p>Enjoy the convenience of finding, booking, and communicating with service providers in one place.</p>
+                <h1><Text string={content?.sec1_heading} /></h1>
+                    <Text string={content?.sec1_detail} />
                 </div>
                 <ul>
-                    <li><Link href="/contact">Contact  Us</Link></li>
-                    <li><Link href="/contact">Terms of use</Link></li>
-                    <li><Link href="/contact">Privacy Policy</Link></li>
+                <li><Link href={content?.sec1_button1_link}><Text string={content?.sec1_button1_text} /></Link></li>
+                    <li><Link href={content?.sec1_button2_link}><Text string={content?.sec1_button2_text} /></Link></li>
+                    <li><Link href={content?.sec1_button3_link}><Text string={content?.sec1_button3_text} /></Link></li>
                 </ul>
             </div>
             <div className="logon_right">
                 <div className="login_header">
                     <div className="logon_logo">
                         <Link href="/">
-                            <img src="images/logo_log.svg" alt="" />
+                        <Image 
+                                src={cmsFileUrl(site_settings?.site_logo)}
+                                width={220}
+                                height={40}
+                                alt={site_settings?.site_name}
+                            />
                         </Link>
                     </div>
                     <div className="btn_blk">
-                        <Link href="" className="site_btn">Become a Professional</Link>
+                    <Link href={content?.right_top_button_link} className="site_btn"><Text string={content?.right_top_button_text} /></Link>
                     </div>
                 </div>
                 <div className="right_inner">
-                    <h2>Create an account</h2>
-                    <p>Enter your details below</p>
+                <h2><Text string={content?.right_sec_heading} /></h2>
+                    <p><Text string={content?.right_sec_tagline} /></p>
                     <form>
                         <div className="form_blk">
                             <input
@@ -77,14 +101,14 @@ export default function Signup() {
                         <div className="have_check">
                             <div className="lbl_btn">
                                 <input type="checkbox" name="remember" id="remember"/>
-                                <label htmlFor="remember">By creating an account you confirm that you agree to our website <Link href="">terms of use</Link> and our <Link href="">privacy notice</Link>.</label>
+                                <label htmlFor="remember"><Text string={content?.check_box_text} /> <Link href="/terms-conditions">terms of use</Link> and our <Link href="/privacy-policy">privacy notice</Link>.</label>
                             </div>
                         </div>
                         <div className="btn_blk">
-                            <button className="site_btn block">Create an Account</button>
+                            <button className="site_btn block"><Text string={content?.button_text} /></button>
                         </div>
                         <div className="question">
-                            <p>Already have an account? <Link href="/login">Sign in</Link></p>
+                            <p><Text string={content?.l_link_tagline} /> <Link href={content?.register_link}><Text string={content?.register_link_text} /></Link></p>
                         </div>
                     </form>
                 </div>
