a2bd489da120b504b9a5448c6fc5c2b877d09e4f
herosolutions94 <herosolutions.tk@gmail.com>
2024-04-22 16:53:09 +0500

    hotjar

--- pages/_document.js
+++ pages/_document.js
@@ -54,6 +54,20 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           `,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:4947019,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+          }}
+        />
         {/* <script
           // eslint-disable-next-line react/no-danger
           dangerouslySetInnerHTML={{
