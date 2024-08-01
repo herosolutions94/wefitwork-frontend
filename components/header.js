b6eafd5c6344c7ffcb79fc1fd8ae046efa26cc59
Muhammad Naeem <naeem@herosolutions.com.pk>
2024-08-01 15:13:43 +0500

    header button repair plans

--- components/header.js
+++ components/header.js
@@ -195,11 +195,11 @@ export default function Header({ siteSettings }) {
               </div>
             </li>
 
-            <li><Link href="/maintenance-cover" onClick={ToggleAction} className="site_btn color">Maintenance Plans</Link></li>
+            <li><Link href="/maintenance-cover" onClick={ToggleAction} className="site_btn color">Repair Plan</Link></li>
           </ul>
         </nav>
         <div className="cell_btn_show">
-          <Link href="/maintenance-cover" className="site_btn color">Maintenance Plans</Link>
+          <Link href="/maintenance-cover" className="site_btn color">Repair Plan</Link>
         </div>
         {/* =============logged user====== */}
         {/* <div className="logged_side">
